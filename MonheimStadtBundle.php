29c07c045c7bd67fd43e45954a9b352ac3b18c0e
Daniel Knoop <daniel.knoop@mum.de>
2025-02-28 10:04:09 +0100

    first commit

---
+++ MonheimStadtBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Monheim\StadtBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
+/**
+ * Stadt Monheim
+ *
+ * @author Daniel Knoop
+ */
+class MonheimStadtBundle extends Bundle
+{
+
+
+
+}
